3e1ba5dcae8093f74899d8cac86274184d1fc6aa
Jonathan <creativeweb.de@gmail.com>
2021-12-29 14:51:34 -0300

    sso using onelogin in rental

--- customer-service/app/Providers/EventServiceProvider.php
+++ customer-service/app/Providers/EventServiceProvider.php
@@ -22,7 +22,7 @@ class EventServiceProvider extends ServiceProvider
             SendEmailVerificationNotification::class,
         ],
     ];
-
+//
     /**
      * Register any events for your application.
      *
